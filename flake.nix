e39bca388d4ea283dbf5d91be4ff50047a53ea8a
psiana011 <psiana011.endo@gmail.com>
2025-11-03 01:14:51 +0900

    Update version to 2.8.0 from 2.7.0

--- flake.nix
+++ flake.nix
@@ -13,44 +13,42 @@
       system: let
         pkgs = import nixpkgs {inherit system;};
 
-        printemps =
-          pkgs.stdenv.mkDerivation
-          {
-            pname = "printemps";
-            version = "2.7.0";
-            src = pkgs.fetchFromGitHub {
-              owner = "snowberryfield";
-              repo = "printemps";
-              rev = "v2.7.0";
-              hash = "sha256-gcP+eOH/WO4HEq10NaEbDKNFFvBrkPoBySvkvOaOgBM=";
-            };
+        printemps = pkgs.stdenv.mkDerivation (finalAttrs: {
+          pname = "printemps";
+          version = "v2.8.0";
 
-            nativeBuildInputs = [
-              pkgs.cmake
-            ];
-            buildInputs = [
-              pkgs.llvmPackages.openmp
-            ];
-
-            dontUseCmakeConfigure = true;
-            buildPhase = ''
-              make -f makefile/Makefile.application
-            '';
-            installPhase = ''
-              mkdir -p $out/bin
-              cp ./build/application/Release/mps_solver $out/bin
-              cp ./build/application/Release/opb_solver $out/bin
-              mkdir -p $out/include
-              cp -r ./printemps/* $out/include
-            '';
-
-            meta = {
-              homepage = "https://snowberryfield.github.io/printemps";
-              mainProgram = "mps_solver";
-              platforms = pkgs.lib.platforms.unix;
-              license = pkgs.lib.licenses.mit;
-            };
+          src = pkgs.fetchFromGitHub {
+            owner = "snowberryfield";
+            repo = "printemps";
+            rev = finalAttrs.version;
+            hash = "sha256-NM669kYXhQdccMBPF7+mqP2aFHn1vgW88BXhNZr3qM4=";
           };
+
+          nativeBuildInputs = [
+            pkgs.cmake
+          ];
+          buildInputs = [
+            pkgs.llvmPackages.openmp
+          ];
+
+          dontUseCmakeConfigure = true;
+          buildPhase = ''
+            make -f makefile/Makefile.application
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ./build/application/Release/printemps $out/bin
+            mkdir -p $out/include
+            cp -r ./printemps/* $out/include
+          '';
+
+          meta = {
+            homepage = "https://snowberryfield.github.io/printemps";
+            mainProgram = "mps_solver";
+            platforms = pkgs.lib.platforms.unix;
+            license = pkgs.lib.licenses.mit;
+          };
+        });
       in {
         packages = {
           inherit printemps;
